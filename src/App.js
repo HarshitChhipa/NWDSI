15bc92cf381984fc4f093628935ab7a3299887af
HarshitChhipa <hchippa@expedien.in>
2019-07-28 23:58:30 +0530

    Created a Basic React app with CRA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div align="center">
+            A Skeleton is created for the application with Create-React-App
+        </div>
+    );
 }
 
 export default App;
