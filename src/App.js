5a74fc17d40a754fff6757d0b5ce4e186fe57649
HarshitChhipa <harshitkankerwal@gmail.com>
2019-07-30 01:05:07 +0530

    Added Folder creation,Forms dummy data and lots of data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import './App.css';
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import Sidebar from './components/Sidebar';
+import '../src/assets/styles/app.scss';
+import reducers from './reducers';
+import {ViewFiles} from './pages/index';
+import generatedummyFileSystem from './utils/dummyFileSystem';
+
+/**
+ * Commeon Store.
+ * @type {any}
+ */
+const store = createStore(
+    reducers,
+    {
+        fileSystem:
+            localStorage.getItem('fileSystem') &&
+            Object.keys(localStorage.getItem('fileSystem')).length > 0
+                ? JSON.parse(localStorage.getItem('fileSystem'))
+                : generatedummyFileSystem()
+    },
+    composeWithDevTools()
+);
 
 function App() {
     return (
-        <div align="center">
-            A Skeleton is created for the application with Create-React-App
-        </div>
+
+        <Provider store={store}>
+            <Router>
+                <BrowserRouter>
+                    <Fragment>
+                        <Sidebar/>
+                        <ViewFiles/>
+                    </Fragment>
+                </BrowserRouter>
+            </Router>
+        </Provider>
+
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
